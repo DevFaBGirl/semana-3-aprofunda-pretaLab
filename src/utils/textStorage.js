2f909c74a6f1da96f8068d79dda383198ea5d4ea
DevFabGirl <107576975+DevFaBGirl@users.noreply.github.com>
2024-09-09 18:50:36 -0300

    Correção do arquivo

--- src/utils/textStorage.js
+++ src/utils/textStorage.js
@@ -1,43 +1,28 @@
-const TextFactory = require('./textFactory')
-const TextStorage = require('../utils/textStorage')
-
-module.exports = {
-  createText: ({ title, content, status, author }) => {
-    const newText = TextFactory.create({ title, content, status, author })
-    TextStorage.add(newText)
-    return newText
-  },
-
-  getAllTexts: () => {
-    return TextStorage.getAll()
-  },
-
-  getTextById: (id) => {
-    return TextStorage.getById(id)
-  },
-
-  updateText: (id, data) => {
-    const text = TextStorage.getById(id)
-
-    if (!text) {
-      return null
+class TextStorage {
+  constructor() {
+    if (!TextStorage.instance) {
+      this.texts = []
+      TextStorage.instance = this
     }
-    if (data.title != null) {
-      text.title = data.title
-    }
-    if (data.content != null) {
-      text.content = data.content
-    }
-    if (data.status != null) {
-      text.status = data.status
-    }
-    if (data.author != null) {
-      text.author = data.author
-    }
-   return text
-  },
-  
-  deleteTextById: (id) => {
-    return TextStorage.deleteText(id)
+    return TextStorage.instance
   }
+
+  add(text) {
+    this.texts.push(text)
+  }
+
+  getAll() {
+    return this.texts
+  }
+
+  getById(id) {
+    return this.texts.find(text => text.id === id)
+  }
+
+  deleteText(id) {
+    return this.texts.filter(text => text.id !== id)
+  }
+
 }
+
+module.exports = new TextStorage()
